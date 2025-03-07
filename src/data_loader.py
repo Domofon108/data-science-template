ed36df0e2af707429d910625b96bce8fc48a2150
Dmitrii Makhov <152228614+Domofon108@users.noreply.github.com>
2025-03-07 16:21:05 +0200

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
+def load_data(filepath):
+    """Loads data into a Pandas DataFrame."""
+    return pd.read_csv(filepath)
+
+if __name__ == "__main__":
+    df = load_data("../data/raw/dataset.csv")
+    print(df.head())
 
